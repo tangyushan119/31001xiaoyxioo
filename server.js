268d57e7d385796093c0d875226d3a0e6296430c
tys <309084785@qq.com>
2026-06-15 18:32:11 +0800

    1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 const hostname = '0.0.0.0';
-const port = 9004;
+const port = 9090;
 
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
